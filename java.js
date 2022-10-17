b995fe9c84f82b280cb672c95266d2f09f56596d
Niva666 <njiva66@gmail.com>
2022-10-17 21:29:04 +0200

    This is the Project as Odin wanted it

--- java.js
+++ java.js
@@ -1,26 +1,38 @@
 let grid = 4; 
-
 const newGrid = document.querySelector('#btn') 
-
 const mom = document.querySelector('.mom'); 
+const resetBtn = document.querySelector('#resetBtn');
 
 const cG = (grid) => {
-for(i=0;i<grid;i++) {
-const child = document.createElement('div');
-child.classList.add('childOne');
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
 
-for(j=0;j<grid;j++){
-    const child2 = document.createElement('div');
-    child2.classList.add('childTwo');
-    child.appendChild(child2)
-    child2.addEventListener('mouseover',function(){
+    for(i=0;i<grid;i++) {
+        const child = document.createElement('div');
+        child.classList.add('childOne');
+
+    for(j=0;j<grid;j++){
+        const widthandHeight = 960 / grid
+        const child2 = document.createElement('div');
+        child2.classList.add('childTwo');
+        child2.style.width = `${widthandHeight}px`
+        child2.style.height = `${widthandHeight}px`
+
+        child2.addEventListener('mouseover',function(){
         child2.style.backgroundColor = 'black';
-    })
+            resetBtn.addEventListener('click', function(){
+                child2.style.backgroundColor = 'rebeccapurple'
+                                        })
+                                    })
+            child.appendChild(child2)
+            }
+        wrapper.appendChild(child);
+    }
+mom.appendChild(wrapper);
 }
-mom.appendChild(child)
- 
-}
-}
+
+cG(grid);
+
 newGrid.addEventListener('click', function(){
     
     let userGrid = Number(prompt('How many squares do you want?'))
@@ -29,7 +41,7 @@ newGrid.addEventListener('click', function(){
         userGrid = Number(prompt("Please enter a value of 100 or lower"))
         
     }
+    const wrapper = document.querySelector('.wrapper')
+    wrapper.remove();
     cG(userGrid);
     })  
-
-cG(grid)
